6550ce072b63221677c523f793a78191c1b4eca2
Hubert Mikołajewicz <hubmikvvv@gmail.com>
2020-10-29 15:13:02 +0100

    Change displaying of x axis values

--- webapp/src/app/dashboard/section/functions/linear-function/linear-function.component.ts
+++ webapp/src/app/dashboard/section/functions/linear-function/linear-function.component.ts
@@ -7,19 +7,25 @@ import { Chart } from "node_modules/chart.js";
   styleUrls: ["./linear-function.component.scss"],
 })
 export class LinearFunctionComponent implements OnInit {
+
   constructor() {}
 
   ngOnInit(): void {
-    this.renderChart(3, 5);
+    this.renderChart(0, 5);
   }
 
-  renderChart(a, b): void{
+  renderChart(a: number, b: number): void {
+
+    var labelsSet = this.fillChartData();
+    for(var i = 0; i < labelsSet.length; i++){
+      labelsSet[i] = i - 15;
+    }
     var dataChart = {
-      labels: [1, 2, 3, 4, 5, 6, 7, 8],
+      labels: labelsSet,
       datasets: [
         {
           label: "f(x) = a*x+b",
-          function: (x: any) => a*x+b,
+          function: (x: any) => a * x + b,
           data: [], // Don't forget to add an empty data array, or else it will break
           borderColor: "rgba(75, 1, 192, 1)",
           fill: false,
@@ -28,31 +34,53 @@ export class LinearFunctionComponent implements OnInit {
     };
 
     Chart.pluginService.register({
-      beforeInit: (chart: { config: { data: any; }; }) => {
-        var data = chart.config.data;
-        for (var i = 0; i < dataChart.datasets.length; i++) {
-          for (var j = 0; j < dataChart.labels.length; j++) {
-            var fct = dataChart.datasets[i].function,
-              x = dataChart.labels[j],
-              y = fct(x);
-            dataChart.datasets[i].data.push(y);
-          }
+      beforeInit: (chart: { config: { data: any } }) => {
+        for (var j = 0; j < dataChart.labels.length; j++) {
+          var fct = dataChart.datasets[0].function,
+            x = dataChart.labels[j],
+            y = fct(x);
+          dataChart.datasets[0].data.push(y);
         }
-      }
+      },
     });
 
     var lineChart = new Chart("linear-function-chart", {
-      type: 'line',
+      type: "line",
       data: dataChart,
       options: {
         scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
+          yAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "Oś Y",
+              },
+              ticks: {
+                beginAtZero: false,
+                min: -15,
+                max: 15,
+              },
+            },
+          ],
+          xAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "Oś X",
+              }
+            },
+          ],
+        },
+      },
     });
   }
+
+  fillChartData(): number[]{
+    var labelsSet = new Array(31);
+    for(var i = 0; i < labelsSet.length; i++){
+      labelsSet[i] = i - 15;
+    }
+    return labelsSet;
+  }
+
 }
